665f64264f9986421e1612175e5449374df972ab
Shashank Yadav <yadavshashank70@gmail.com>
2022-05-14 02:57:05 +0530

    Word count and time to read bug is Fixed also the CLS cummulative layout shift due to alert is reduced to zero

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Alert(props) {
-
-  var toupper=(word)=>
-  {
-    var capitalise=word.toLowerCase();
+  var toupper = (word) => {
+    var capitalise = word.toLowerCase();
     return capitalise.charAt(0).toUpperCase() + capitalise.slice(1);
-  }
+  };
   return (
-props.alertt && <div className={`alert alert-${props.alertt.type} fade show`} role="alert">
-  <strong> {toupper(props.alertt.type)} </strong> : {props.alertt.msg} 
-  
-</div>  )
+    <div style={{height: '45px'}}>
+    {props.alertt &&
+    <div
+      className={`alert alert-${props.alertt.type} fade show`}
+      role="alert"
+    >
+      <strong> {toupper(props.alertt.type)} </strong> : {props.alertt.msg}
+    </div>}
+    </div>
+  );
 }
